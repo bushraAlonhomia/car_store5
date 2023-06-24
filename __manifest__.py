1c258267f1edc221b26abd5d0020300f2696b69a
bushraAlonhomia <bushraalisalhe@gmail.com>
2023-06-24 13:31:26 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Car Store',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'Salsaabel Techon Solutions',
+  'website': "",
+  'company': 'Salsaabel Techon Solutions',
+  'maintainer': 'Salsaabel Techon Solutions',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/ir.model.access.csv',
+      'views/car_store_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
